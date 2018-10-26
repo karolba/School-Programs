0c7620830d5fbc5cf9a2a61fe56c73badd2dd673
Karol Baraniecki <karol@baraniecki.eu>
2018-10-26 16:22:34 +0200

    refactor

--- timers/NewTimer.java
+++ timers/NewTimer.java
@@ -1,6 +1,17 @@
 public class NewTimer {
-    private int seconds;
+    private int seconds = 0;
     static int createdTimers = 0;
+
+    private class TimeDate {
+        public int hours;
+        public int minutes;
+        public int seconds;
+        public TimeDate(int s) {
+            hours = s / 3600 % 24;
+            minutes = (s / 60) % 60;
+            seconds = s % 60;
+        }
+    }
 
     NewTimer() {
         createdTimers++;
@@ -13,90 +24,48 @@ public class NewTimer {
     NewTimer(int h, int m, int s) {
         createdTimers++;
         System.out.print("make new Timer with ");
-        seconds = s + 60*m + 3600*h;
-
-        setRightTime(converter());
+        setSeconds(s + 60 * m + 3600 * h);
 
         System.out.println(this);
     }
 
-    private int [] converter() {
-        int [] output = new int[3];
+    private void setSeconds(int s) {
+        seconds = s % (60 * 60 * 24); // limit to one day
 
-        output[0] = seconds / 3600;
-        output[1] = (seconds-output[0]*3600)/60;
-        output[2] = seconds - output[0]*3600 - output[1]*60;
-
-        return output;
+        // roll-back prevention. Emulates a modulo operator because the % in Java is a remainder operator.
+        if(seconds < 0)
+            seconds = (60 * 60 * 24) + seconds;
     }
 
-    private int setRightTime(int[] time) {
-        if(time[2] < 0) {
-            time[1]--;
-            time[2] += 60;
-        }
-
-        if(time[2] >= 60) {
-            time[1]++;
-            time[2] -= 60;
-        }
-
-        if(time[1] < 0) {
-            time[0]--;
-            time[1] += 60;
-        }
-
-        if(time[1] >= 60) {
-            time[0]++;
-            time[1] -= 60;
-        }
-
-        if(time[0] < 0) {
-            time[0] += 24;
-        }
-
-        if(time[0] >= 24) {
-            time[0] -= 24;
-        }
-        return time[0]*3600 + time[1]*60 + time[2];
-    }
 
     public String toString() {
-        int [] time = converter();
+        TimeDate td = new TimeDate(seconds);
 
-        return "time : " + time[0] + ":" + time[1] + ":" + time[2];
+        return "time : " + td.hours + ":" + td.minutes + ":" + td.seconds;
     }
 
     boolean equals(int h, int m, int s) {
         System.out.print(this + " is equal to " + h + ":" + m + ":" + s + " ? ");
 
-        int [] time = converter();
+        TimeDate td = new TimeDate(seconds);
 
-        return (h == time[0] && m == time[1] && s == time[2]);
+        return h == td.hours && m == td.minutes && s == td.seconds;
     }
 
     void hoursIncrement() {
         System.out.println("hours increment ...");
 
         seconds += 3600;
-
-        seconds = setRightTime(converter());
     }
-
-
 
     void hoursDecrement() {
         System.out.println("hours decrement ...");
 
         seconds -= 3600;
-
-        seconds = setRightTime(converter());
     }
 
     void secondsIncrement() {
         seconds += 1;
-
-        seconds = setRightTime(converter());
     }
 
     void secondsIncrement(int n) {
@@ -114,33 +83,19 @@ public class NewTimer {
     }
 
     int compare(int h, int m, int s) {
-          System.out.println(this + " compare with " + h + ":" + m + ":" + s);
+        System.out.println(this + " compare with " + h + ":" + m + ":" + s);
 
         System.out.println("1 > , -1 <, 0 ==");
 
-        int output;
-        int [] time = converter();
+        int otherSeconds = 3600 * h + 60 * m + s;
 
-        if(time[0] > h) {
-            output = 1;
-        } else if(time[0] < h) {
-            output = -1;
-        } else {
-            if(time[1] > m) {
-                output = 1;
-            } else if(time[1] < m) {
-                output = -1;
-            } else {
-                if(time[2] > s) {
-                    output = 1;
-                } else if(time[2] < s) {
-                    output = -1;
-                } else {
-                    output = 0;
-                }
-            }
+        int difference = seconds - otherSeconds;
 
-        }
-        return output;
+        if(difference < -1)
+            difference = -1;
+        else if(difference > 1)
+            difference = 1;
+
+        return difference;
     }
 }
